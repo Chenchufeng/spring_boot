9eb973d7f22b3cab4455cb6cb48c139cee085ad4
chenchufeng <437121948@qq.com>
2021-01-10 12:35:00 +0800

    my spring_study

---
+++ src/main/java/com/example/my_springboot_study/MySpringbootStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.my_springboot_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringbootStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringbootStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/my_springboot_study/MySpringbootStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my_springboot_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringbootStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
