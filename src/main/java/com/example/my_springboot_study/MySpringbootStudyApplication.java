67caa67bdcf394e40a3ad2b64edbd519254bd581
chenchufeng <437121948@qq.com>
2021-01-11 20:59:40 +0800

    use templates

--- src/main/java/com/example/my_springboot_study/MySpringbootStudyApplication.java
+++ src/main/java/com/example/my_springboot_study/MySpringbootStudyApplication.java
@@ -3,6 +3,8 @@ package com.example.my_springboot_study;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@SpringBootApplication标注这个类是springboot的一个应用
+//@SpringBootConfiguration springboot的配置
 @SpringBootApplication
 public class MySpringbootStudyApplication {
 
--- src/test/java/com/example/my_springboot_study/MySpringbootStudyApplicationTests.java
+++ src/test/java/com/example/my_springboot_study/MySpringbootStudyApplicationTests.java
@@ -1,13 +1,23 @@
 package com.example.my_springboot_study;
 
+import com.example.my_springboot_study.pojo.Person;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MySpringbootStudyApplicationTests {
 
+    @Autowired
+    Person person;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testPerson(){
+        System.out.println(person);
     }
 
 }
